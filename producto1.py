eec2d8023aa418bb9992b95e4b65f973b5fc808b
franco1610 <fciscoag85@gmail.com>
2023-09-09 18:32:18 -0500

    producto1

--- producto1.py
+++ producto1.py
@@ -1,28 +1,59 @@
-ciclo = 1  
-clave=0 
-dic={} 
-while ciclo == 1:  
-    print("********** PELUQUERIA PETS **********")  
-    opcion=int(input("Seleccione una opción: \n1. Ingresar \n2. imprimir  \n3. Eliminar \n4. Editar \n5. Salir\n")) 
-    if opcion == 1: 
-        clave=clave+1 
-        cliente=[None,None,None,None] 
-        cliente[0]=input(("Ingrese el nombre del amo: ")) 
-        cliente[1]=input(("Ingrese nombre de la mascota: ")) 
-        cliente[2]=input(("Ingrese el tipo de la mascota: ")) 
-        cliente[3]=input(("Ingrese la raza de la mascota: ")) 
-        dic[clave]=cliente
-    elif opcion ==2: 
-        for clave in dic:
-            print(f"-{clave} :{dic[clave]}")
-    elif opcion==3: 
+ciclo = 1
+lista = []
+
+while ciclo == 1:
+    print("")
+    print("********** VETERINARIA PETS **********")
+    opcion = int(input("Seleccione una opción: \n1. Ingresar \n2. Imprimir \n3. Eliminar \n4. Editar \n5. Salir"))
+    print("")
+
+    if opcion == 1:
+        cliente = {"Dueno": "", "Mascota": "", "Animal": "", "Raza": ""}
+        cliente["Dueno"] = input("Ingrese el nombre del amo: ").capitalize()
+        cliente["Mascota"] = input("Ingrese nombre de la mascota: ").capitalize()
+        cliente["Animal"] = input("Ingrese el tipo de la mascota: ").capitalize()
+        cliente["Raza"] = input("Ingrese la raza de la mascota: ").capitalize()
+        lista.append(cliente)
+    elif opcion == 2:  # Imprimir
+        for i, elemento in enumerate(lista, start=1):
+            print(f"{i}. {elemento}")
+    elif opcion == 3:  # Eliminar
+        for k, elemento in enumerate(lista, start=1):
+            print(f"{k}. {elemento}")
         eliminar=int(input("Ingrese el numero de lista a eliminar")) 
-        dic.pop(eliminar) 
-    elif opcion==5: 
-        print("usted eligió salir") 
-        ciclo=2   
-        break 
+        lista.pop(eliminar-1) 
+        print("Cliente eliminado. ")
+    elif opcion == 4:
+        for j, elemento in enumerate(lista, start=1):
+            print(f"{j}. {elemento}")
+        print('')
+        opciondelistacliente = int(input("Ingrese el número de lista a editar: "))
+        opcionedi2 = int(input("Seleccione cuál quiere editar: \n1. Amo \n2. Mascota  \n3. Animal\n4. Raza \n5. Salir\n"))
+        if opcionedi2 < 1 or opcionedi2 > 5:
+            print("Opción no válida")
+        else:
+            if opcionedi2 == 5:
+                print("Usted eligió salir del sistema")
+            else:
+                nuevodato = input("Ingrese el nuevo dato: ")
+                ubicacion = opciondelistacliente - 1
+                clienteeditar = lista[ubicacion]
+                if opcionedi2 == 1:
+                    clienteeditar['Dueno'] = nuevodato
+                    print("Dato actualizado!")
+                elif opcionedi2 == 2:
+                    clienteeditar['Mascota'] = nuevodato
+                    print("Dato actualizado!")
+                elif opcionedi2 == 3:
+                    clienteeditar['Animal'] = nuevodato
+                    print("Dato actualizado!")
+                elif opcionedi2 == 4:
+                    clienteeditar['Raza'] = nuevodato
+                    print("Dato actualizado!")
+                lista[ubicacion] = clienteeditar
+                print(clienteeditar)
+    elif opcion == 5:  # Salir
+        print("Usted eligió salir")
+        break
 
-print("Usted ha salido del sistema")  
-
-print("")  
+print("Usted ha salido del sistema")
--- prueba.py
+++ prueba.py
@@ -1,28 +1,61 @@
-ciclo = 1  
-lista=[]
-while ciclo == 1:  
-    print("") 
-    print("********** VETERINARIA PETS **********")  
-    opcion=int(input("Seleccione una opción: \n1. Ingresar \n2. imprimir  \n3. Eliminar \n4. Editar \n5. Salir\n")) 
-    if opcion == 1:  
-        cliente={"Dueno":"","Mascota":"","Animal":"","Raza":""}
-        cliente["Dueno"]= input(("Ingrese el nombre del amo: ")).capitalize() 
-        cliente["Mascota"]=input(("Ingrese nombre de la mascota: ")).capitalize()
-        cliente["Animal"]=input(("Ingrese el tipo de la mascota: ")).capitalize() 
-        cliente["Raza"]=input(("Ingrese la raza de la mascota: ")).capitalize()
+ciclo = 1
+lista = []
+
+while ciclo == 1:
+    print("")
+    print("********** VETERINARIA PETS **********")
+    opcion = int(input("Seleccione una opción: \n1. Ingresar \n2. Imprimir \n3. Eliminar \n4. Editar \n5. Salir"))
+    print("")
+
+    if opcion == 1:
+        cliente = {"Dueno": "", "Mascota": "", "Animal": "", "Raza": ""}
+        cliente["Dueno"] = input("Ingrese el nombre del amo: ").capitalize()
+        cliente["Mascota"] = input("Ingrese nombre de la mascota: ").capitalize()
+        cliente["Animal"] = input("Ingrese el tipo de la mascota: ").capitalize()
+        cliente["Raza"] = input("Ingrese la raza de la mascota: ").capitalize()
         lista.append(cliente)
-    elif opcion ==2: 
-        for i, lista in enumerate(lista, start=1):
-            print(f"{i}. {lista}")
-    elif opcion==3: 
-        eliminar=int(input("Ingrese el numero de lista a eliminar")) 
-        lista.pop(eliminar) 
-        print("Cliente eliminado. ")     
-    elif opcion==5: 
-        print("usted eligió salir") 
-        ciclo=2   
-        break 
+    elif opcion == 2:  # Imprimir
+        for i, elemento in enumerate(lista, start=1):
+            print(f"{i}. {elemento}")
+    elif opcion == 3:  # Eliminar
+        try:
+            eliminar = int(input("Ingrese el número de lista a eliminar: "))
+            if 1 <= eliminar <= len(lista):
+                lista.pop(eliminar - 1)
+                print("Cliente eliminado.")
+            else:
+                print("Número de lista no válido.")
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
+    elif opcion == 4:
+        for j, elemento in enumerate(lista, start=1):
+            print(f"{j}. {elemento}")
+        print('')
+        opciondelistacliente = int(input("Ingrese el número de lista a editar: "))
+        opcionedi2 = int(input("Seleccione cuál quiere editar: \n1. Amo \n2. Mascota  \n3. Animal\n4. Raza \n5. Salir\n"))
+        if opcionedi2 < 1 or opcionedi2 > 5:
+            print("Opción no válida")
+        else:
+            if opcionedi2 == 5:
+                print("Usted eligió salir del sistema")
+            else:
+                nuevodato = input("Ingrese el nuevo dato: ")
+                ubicacion = opciondelistacliente - 1
+                clienteeditar = lista[ubicacion]
+                if opcionedi2 == 1:
+                    clienteeditar['Dueno'] = nuevodato
+                    print("Dato actualizado!")
+                elif opcionedi2 == 2:
+                    clienteeditar['Mascota'] = nuevodato
+                    print("Dato actualizado!")
+                elif opcionedi2 == 3:
+                    clienteeditar['Animal'] = nuevodato
+                elif opcionedi2 == 4:
+                    clienteeditar['Raza'] = nuevodato
+                lista[ubicacion] = clienteeditar
+                print(clienteeditar)
+    elif opcion == 5:  # Salir
+        print("Usted eligió salir")
+        break
 
-print("Usted ha salido del sistema")  
-
-print("")
+print("Usted ha salido del sistema")
